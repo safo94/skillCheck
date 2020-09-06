355d501530d4142b0628f6ef1dbebd7df733a32b
Boston Ivy Tech <bostonivytech@Bostons-iMac.local>
2020-09-06 11:20:46 -0400

    verifying models

--- selenium-springboot-cucumber/src/test/java/com/example/selenium/pages/ShopNewPage.java
+++ selenium-springboot-cucumber/src/test/java/com/example/selenium/pages/ShopNewPage.java
@@ -5,11 +5,27 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class ShopNewPage implements BasePage {
 
     @FindBy(how = How.CSS, using = "[class='btn btn-primary btn-md btn-block']")
     public WebElement shopNewSearch;
 
+    @FindBy(how = How.CSS, using = "[data-qa='ModelSelect']")
+    public List<WebElement> carModels;
+
+    @FindBy(how = How.CSS, using = "[data-test='modelSearchBrand']")
+    public List<WebElement> carMakes;
+
+    public List<String> getTextFromWebElement(){
+        List<String> textFromListWeb = new ArrayList<>();
+        for (WebElement carModel : carModels) {
+            textFromListWeb.add(carModel.getText().split("/n")[0]);
+        }
+        return textFromListWeb;
+    }
 
 }
--- selenium-springboot-cucumber/src/test/java/com/example/selenium/steps/ShopNewSteps.java
+++ selenium-springboot-cucumber/src/test/java/com/example/selenium/steps/ShopNewSteps.java
@@ -1,9 +1,12 @@
 package com.example.selenium.steps;
 
 import com.example.selenium.pages.ShopNewPage;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class ShopNewSteps {
 
@@ -11,8 +14,37 @@ public class ShopNewSteps {
     private ShopNewPage shopNewPage;
 
     @Given("I shop for new car")
-    public void i_shop_for_a_car() {
+    public void i_shop_for_a_car() throws InterruptedException {
         shopNewPage.shopNewSearch.click();
+    }
+
+    @Then("I verify there are {int} car makes")
+    public void i_verify_there_are_car_makes(int numberMakes) {
+        List<WebElement> carMakes = shopNewPage.carMakes;
+        Assert.assertEquals(numberMakes, carMakes.size());
+    }
+
+    @Then("I select make {string}")
+    public void i_select_make(String make) {
+        List<WebElement> carMakes = shopNewPage.carMakes;
+        for (int i = 0; i < carMakes.size() ; i++) {
+            if(carMakes.get(i).getText().equals(make)){
+                carMakes.get(i).click();
+                break;
+            }
+        }
+    }
+
+    @Then("I verify car models are {string}")
+    public void i_verify_car_models_are(String models) throws InterruptedException {
+        Thread.sleep(2000);
+        String modelsArr [] = models.split(",");
+        List<String> actualCarModels = shopNewPage.getTextFromWebElement();
+        for (int i = 0; i <modelsArr.length ; i++) {
+            Assert.assertTrue("Failed model: "+modelsArr[i],
+                    actualCarModels.contains(modelsArr[i]));
+        }
+
     }
 
 }
